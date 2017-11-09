f35571e2ed11848e750800dc4d32593fd010ce1b
crazywolf <crazywolf132@gmail.com>
2017-11-09 15:03:28 +1100

    Standard added to the internet module.

--- mods/internet.js
+++ mods/internet.js
@@ -1,13 +1,13 @@
 module.exports=(function(Asher){
   Asher.addResponder(/(Find information about |Tell me about |What is\s?[an]*\s(?!the)|What are )([^\d]+)/i, function(){
     var query = RegExp.$2
-    Asher.request('http://api.duckduckgo.com/?q=Where+is+'+query+'&format=json&pretty=1', function (error, response, body) {
+    Asher.request(`http://api.duckduckgo.com/?q=Where+is+`+query+`&format=json&pretty=1`, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var body = JSON.parse(body);
         if ( body.Abstract ){
           Asher.respond(body.Abstract);
         }else{
-          Asher.respond("I'm sorry I couldn't find any information about "+query)
+          Asher.respond(`I'm sorry I couldn't find any information about `+query)
         }
       }
     })
