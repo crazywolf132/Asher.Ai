0b49596b08c6fc26315dbc5bd53a802f555ff919
crazywolf <crazywolf132@gmail.com>
2017-11-09 19:47:22 +1100

    Creating core module again...

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require(`./mods/math.js`)(Asher);
 require(`./mods/internet_query.js`)(Asher);
 require(`./mods/natural-language.js`)(Asher);
 
-require(`./mods/hello-test.js`)(Asher);
+require(`./mods/core.js`)(Asher);
 
 var express=require(`express`);
 var app=express();
