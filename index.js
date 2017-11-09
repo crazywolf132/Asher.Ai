b726d8aa162e354256b02362048372bc02576587
crazywolf <crazywolf132@gmail.com>
2017-11-10 10:43:28 +1100

    Added GetToken function.

--- index.js
+++ index.js
@@ -42,6 +42,19 @@ api_router.use(function(req, res, next) {
 	next();
 });
 
+getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
 api_router.route(`/login`)
 		.post(function(req, res){
 			User.findOne({
