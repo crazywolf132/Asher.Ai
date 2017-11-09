47f84176c9a4738e18e9f8b0c76727335c77aa99
crazywolf <crazywolf132@gmail.com>
2017-11-09 18:03:35 +1100

    Updated internet.js to new code structure.

--- mods/internet.js
+++ mods/internet.js
@@ -1,15 +1,40 @@
 module.exports=(function(Asher){
-  Asher.addResponder(/(Find information about |Tell me about |What is\s?[an]*\s(?!the)|What are )([^\d]+)/i, function(){
-    var query = RegExp.$2
-    Asher.request(`http://api.duckduckgo.com/?q=Where+is+`+query+`&format=json&pretty=1`, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var body = JSON.parse(body);
-        if ( body.Abstract ){
-          Asher.respond(body.Abstract);
-        }else{
-          Asher.respond(`I'm sorry I couldn't find any information about `+query)
+    Asher.addCommand("query",function(args){
+        try{
+            var question=args[0];
+        }catch(e){
+            return {
+                status:"fail",
+                error:"No question given!"
+            };
         }
-      }
-    })
-  })
+        var expression=/(Find information about |Tell me about |What is\s?[an]*\s(?!the)|What are )([^\d]+)/i;
+        var query=expression.exec(question);
+        try{
+            var search=query[0];
+        }catch(e){
+            return {
+                status:"fail",
+                error:"Please provide a better question!"
+            };
+        }
+        search=serch.replace(/\s/gim,"%20"); // replace spaces with the encoded url space
+        var request_url=`http://api.duckduckgo.com/?q=Where%20is%20${search}&format=json&pretty=1`;
+        Asher.request(request_url,function(error,response,body){
+            if(!error&&respnase.statusCode===200){
+                var body = JSON.parse(body);
+                if ( body.Abstract ){
+                    return {
+                        status:"success",
+                        result:body.Abstract
+                    };
+                }else{
+                    return {
+                        status:"fail",
+                        error:`I'm sorry I couldn't find any information about "${question}"`
+                    };
+                }
+            }
+        });
+    });
 });
