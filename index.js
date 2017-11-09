cb61ce84a9ee80e575d6ffae28509bce904019d3
Crazywolf132 <crazywolf132@gmail.com>
2017-11-09 12:42:45 +1100

    Fixed to allow for the server to accept incomming messages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // BASE SETUP
 // =============================================================================
-console.log(process.env.PORT || 8080);
+console.log(process.env.PORT || 80);
 // call the packages we need
 var express    = require('express');
 var bodyParser = require('body-parser');
@@ -14,7 +14,7 @@ app.use(morgan('dev')); // log requests to the console
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port     = process.env.PORT || 8080; // set our port
+var port     = process.env.PORT || 80; // set our port
 
 
 // ROUTES FOR OUR API
