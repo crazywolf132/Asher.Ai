0e9905bbd9d7755eebc89501d88f3ef421b38cab
crazywolf132 <crazywolf132@gmail.com>
2017-11-09 12:23:27 +1100

    Cleaning up the code a little.
    Adding in the tab so it is easier to read.

--- mods/core.js
+++ mods/core.js
@@ -1,40 +1,40 @@
 module.exports=(function(Asher){
   
-Asher.addResponder(/^Hi|hello|hey/i, function(){
-  Asher.respond('Hello')
-})
+  Asher.addResponder(/^Hi|hello|hey/i, function(){
+    Asher.respond('Hello')
+  })
 
-Asher.addResponder(/^Thanks?\s?(you)?/i, function(){
-  Asher.respond('You are welcome')
-})
+  Asher.addResponder(/^Thanks?\s?(you)?/i, function(){
+    Asher.respond('You are welcome')
+  })
 
-Asher.addResponder(/My name is (\w+)/i, function(){
-  var name = RegExp.$1;
-  storeName(name);
-  Asher.respond("Hello "+name+", It's nice to meet you.")
-})
+  Asher.addResponder(/My name is (\w+)/i, function(){
+    var name = RegExp.$1;
+    storeName(name);
+    Asher.respond("Hello "+name+", It's nice to meet you.")
+  })
 
-Asher.addResponder(/Call me (\w+)/i, function(){
-  var name = RegExp.$1;
-  storeName(name);
-  Asher.respond("Ok I will call you "+name+" from now on")
-})
+  Asher.addResponder(/Call me (\w+)/i, function(){
+    var name = RegExp.$1;
+    storeName(name);
+    Asher.respond("Ok I will call you "+name+" from now on")
+  })
 
-Asher.addResponder(/What's my name/i, function(){
-  var name = Asher.db.get('users_name');
-  if ( name ){
-    Asher.respond('Your name is '+name)
-  }else{
-    Asher.respond("You haven't told me your name")
+  Asher.addResponder(/What's my name/i, function(){
+    var name = Asher.db.get('users_name');
+    if ( name ){
+      Asher.respond('Your name is '+name)
+    }else{
+      Asher.respond("You haven't told me your name")
+    }
+  })
+
+  Asher.addResponder(/how are you/i, function(){
+    Asher.respond('I\'m good, thanks for asking!')
+  })
+
+  function storeName(name){
+    Asher.db.set('users_name', name);
   }
-})
-
-Asher.addResponder(/how are you/i, function(){
-  Asher.respond('I\'m good, thanks for asking!')
-})
-
-function storeName(name){
-  Asher.db.set('users_name', name);
-}
 
 });
