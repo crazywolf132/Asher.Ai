1dab27dcffde365d3c40928171e1a51a8b97563b
crazywolf <crazywolf132@gmail.com>
2017-11-10 10:27:10 +1100

    Added USER-SCHEMA

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ mongoose.connect(config.database);
 app.use(morgan(`dev`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(passport.initialize());
 
 var port=process.env.PORT||80;
 
