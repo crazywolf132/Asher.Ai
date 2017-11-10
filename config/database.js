b2af2e7a5a6d39ce12adeec167cffb237b37f564
Emma <admin@cryogena.net>
2017-11-10 00:48:58 -0700

    new index

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-module.exports = {
-  'secret':'ILoveBigFlufflyPandas',
-  'database': 'mongodb://localhost/AsherApi'
+module.exports={
+    'secret':'ILoveBigFlufflyPandas',
+    'database':'mongodb://localhost/AsherApi'
 };
