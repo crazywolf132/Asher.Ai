784a02696b51215d3e1174c7dd27acabd70b884f
crazywolf <crazywolf132@gmail.com>
2017-11-11 22:54:20 +1100

    Ok. Up to the stage where we can start to insert it in.

--- core/core.js
+++ core/core.js
@@ -15,6 +15,21 @@ Core = ( function() {
     console.log('Asher has finished learning, time to listen...');
   })
 
+  AsherHear = ( function(msg) {
+    var interpretation = Asher.Brain.interpret(msg);
+    console.log('Asher heard: ' + msg);
+    console.log('Asher interpretation: ', interpretation);
+    if (interpretation.guess){
+      console.log('Invoking skill: ' + interpretation.guess);
+      Asher.Brain.invoke(interpretation.guess, interpretation, msg);
+    } else {
+      // This is where we need to respond with a message saying "oh no... idk what to do."
+      fs.appendFile('phrase-errors.txt', msg, function (err) {
+        console.log('\n\tBrain Err: Appending phrase for review\n\t\t' + msg)
+      })
+    }
+  })
+
   function eachKey(object, callback) {
     Object.keys(object).forEach(function(key) {
       callback(key, object[key]);
