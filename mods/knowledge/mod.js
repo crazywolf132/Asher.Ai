7e64d79f7c40333581e6a93e07a6b7e738bea495
crazywolf <crazywolf132@gmail.com>
2018-01-01 20:12:45 +1100

    Adding some comments to fix errors

--- mods/knowledge/mod.js
+++ mods/knowledge/mod.js
@@ -2,6 +2,8 @@ module.exports=(
   function(subject, message) {
     input = message.split(' ');
     let query = input[input.length - 1];
+    console.log(input)
+    console.log("Query is: " + query)
     var request = require('request');
     var msg = ""
     request.post('http://api.duckduckgo.com/?q=Where+is+'+query+'&format=json&pretty=1', (err, resp, body) => {
