70cbeea65b5419d0e4de449fcc1a7350458b186b
crazywolf <crazywolf132@gmail.com>
2018-01-01 20:03:53 +1100

    Removed un-needed code

--- mods/knowledge/mod.js
+++ mods/knowledge/mod.js
@@ -4,7 +4,7 @@ module.exports=(
     let query = input[-1];
     var request = require('request');
     var msg = ""
-    request.post('http://api.duckduckgo.com/?q=Where+is+'+query+'&format=json&pretty=1', { json: { command: _message } }, (err, resp, body) => {
+    request.post('http://api.duckduckgo.com/?q=Where+is+'+query+'&format=json&pretty=1', (err, resp, body) => {
         //do stuff
         if (!err && resp.statusCode == 200) {
             console.log(body)
