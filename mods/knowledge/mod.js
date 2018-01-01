8a2b5a742eecdb291042348afc4d8a5bdbcfe436
crazywolf <crazywolf132@gmail.com>
2018-01-01 20:24:36 +1100

    trying to access function outside of export

--- mods/knowledge/mod.js
+++ mods/knowledge/mod.js
@@ -10,21 +10,22 @@ module.exports=(
     })
   }
 
-  function ask(query){
-    return new Promise(resolve => {
-          request.post('http://api.duckduckgo.com/?q=Where+is+'+query+'&format=json&pretty=1', (err, resp, body) => {
-              //do stuff
-              if (!err && resp.statusCode == 200) {
-                  console.log(body)
-                  var body = JSON.parse(body);
-                  //{ status: 'success', reply: 92 }
-                  if ( body.Abstract ){
-                    resolve(body.Abstract);
-                  }else{
-                    resolve("I'm sorry I couldn't find any information about " + query);
-                  }
-              }
-          })
-      })
-  }
+
 )
+function ask(query){
+  return new Promise(resolve => {
+        request.post('http://api.duckduckgo.com/?q=Where+is+'+query+'&format=json&pretty=1', (err, resp, body) => {
+            //do stuff
+            if (!err && resp.statusCode == 200) {
+                console.log(body)
+                var body = JSON.parse(body);
+                //{ status: 'success', reply: 92 }
+                if ( body.Abstract ){
+                  resolve(body.Abstract);
+                }else{
+                  resolve("I'm sorry I couldn't find any information about " + query);
+                }
+            }
+        })
+    })
+}
