bde65441f21519864739873d6f76804ccb9ab344
crazywolf <crazywolf132@gmail.com>
2018-01-03 14:07:42 +1100

    Fixed errors with toLoad

--- server.js
+++ server.js
@@ -134,12 +134,12 @@ workItOut = function(msg, usedSocket) {
     // This is used for testing matches... to see if we can match a string to an example from a `words.txt` file.
     //let _testy = nlp('whats 5 divide 5').match('whats #Value (plus|minus|divide|times) .? #Value .?').found
 
-    getMod(mods, _mod_types, _questionType, msg)
+    getMod(mods, _mod_types, _questionType, msg, toLoad)
     if (inArray(toLoad, socketMods) && !usedSocket) {
       return 'Sorry, to use this module. You need to connect to the server via socket.'
     }
     if (toLoad === '' && _questionType != 'other'){
-      getMod(mods, _mod_types, 'other', msg)
+      getMod(mods, _mod_types, 'other', msg, toLoad)
       if (toLoad === ''){
         return 'I am horribly sorry, but i just dont know what to respond...'
       }
@@ -154,7 +154,7 @@ workItOut = function(msg, usedSocket) {
     return (_mod_to_run(sub, msg));
 }
 
-getMod = function(_mods, _modTypes, _questionType, _msg){
+getMod = function(_mods, _modTypes, _questionType, _msg, _toLoad){
   _mods.forEach(function(mod){
     if (_modTypes[mod] === _questionType){
       _ins = []
