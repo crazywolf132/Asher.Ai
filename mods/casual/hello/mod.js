1b01cddee4170570d2ec26fe3b5c9e2983967d53
Jamie Nevada <jameskarltonstone@gmail.com>
2018-01-05 23:25:32 -0700

    return async

--- mods/casual/hello/mod.js
+++ mods/casual/hello/mod.js
@@ -1,1 +1,1 @@
-module.exports = (() => { const responses = ['hello', 'hey', "G'day", 'hi', 'howdy', 'aloha']; return (responses[Math.floor(Math.random() * responses.length - 1)]); });
+module.exports = (async () => { const responses = ['hello', 'hey', "G'day", 'hi', 'howdy', 'aloha']; return (responses[Math.floor(Math.random() * responses.length - 1)]); });
