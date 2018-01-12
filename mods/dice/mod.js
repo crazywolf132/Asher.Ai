a0b1d4fdb5ff0f68f701a74609c8f73fbfbc8b8e
crazywolf <crazywolf132@gmail.com>
2018-01-12 11:49:52 +1100

    Fixed the dice mod

--- mods/dice/mod.js
+++ mods/dice/mod.js
@@ -1,4 +1,6 @@
 module.exports = (async (subject, message, socket) => {
-    choices = ['heads', 'tales']
-    return (choices[Math.floor(Math.random() * choices.length - 1)]);
+    let msg = nlp(message).match('#Value sided').out('text')
+    let holder =  msg.split(' ');
+    let num = holder[0]
+    return (Math.floor((Math.random() * num) + 1););
 });
