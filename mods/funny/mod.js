29f1501491aaf41bfae92d42e74bb059b46bae85
crazywolf132 <crazywolf132@gmail.com>
2019-02-04 16:20:12 +1100

    Updated funny-mod to ES6 imports

--- mods/funny/mod.js
+++ mods/funny/mod.js
@@ -1,4 +1,4 @@
-module.exports.core = (core) => {
+export function core(core) {
     let words = ["tell .? joke", ".? tell .? joke", ".? humour .?", "how about a joke .?", "make .? laugh .?", "joke around .?"];
     core.Asher.hear(words, (payload, chat) => {
       jokes = ["I wrote a song about a tortilla. Well actually, it’s more of a wrap.", "“Um.” — First horse that got ridden", "Some people just have a way with words, and other people … oh … not have way."];
