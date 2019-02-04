5774262f01858b7e81f3fe7b61ec55fe3f6bd535
crazywolf132 <crazywolf132@gmail.com>
2019-02-04 16:19:32 +1100

    Updated dice-mod to ES6 imports

--- mods/dice/mod.js
+++ mods/dice/mod.js
@@ -1,4 +1,4 @@
-module.exports.core = (core) => {
+export function core(core) {
 	let words = [`roll a .?`, `roll (a|an) #Value *?`, `.? a (dice|die)`, `roll a dice`, `throw a dice`];
 	core.Asher.hear(words, (payload, chat) => {
 		chat.say(Math.floor(Math.random() * 6 + 1));
