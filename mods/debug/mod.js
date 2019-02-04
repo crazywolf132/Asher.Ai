5905d586b974862b8874330fcba1bb08aefd2ad4
crazywolf132 <crazywolf132@gmail.com>
2019-02-04 16:19:02 +1100

    Updated debug-mod to ES6 imports

--- mods/debug/mod.js
+++ mods/debug/mod.js
@@ -1,6 +1,6 @@
-module.exports.core = core => {
+export function core(core) {
   core.Asher.hear("debug me", (payload, chat) => {
     chat
       .say("```" + JSON.stringify(core.Asher.actions) + "```");
   });
-};
+}
