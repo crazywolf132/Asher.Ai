2864e1259a874ec89df395c656d119f0f523c6bb
crazywolf <crazywolf132@gmail.com>
2019-03-15 00:42:54 +1100

    Core functions of loading brain added.
    This means that the brain can now load from the training data provided.
    
    Just need to work on saving the brain, the back links, and responses

--- core/functions/brain.cpp
+++ core/functions/brain.cpp
@@ -6,14 +6,16 @@
 #include <iterator>
 #include <sstream>
 #include <algorithm>
-#include <time.h>
 
 using namespace std;
 
 map< string, vector<string> > associationsDB;
 
-string getResponse(string inputSentance) {
-    return "";
+typedef std::vector<std::string> stringvec;
+
+int random_int(int min, int max)
+{
+   return rand()%(max-min+1) + min;
 }
 
 string trim(const string& str, const string& whitespace = " \t", bool returnWhole = false) {
@@ -28,23 +30,44 @@ string trim(const string& str, const string& whitespace = " \t", bool returnWhol
     return str.substr(strBegin, strRange);
 }
 
-bool inMap(const string& str, const map<string, vector<string>>& m) {
+bool inMap(const string& str, const map<string, stringvec>& m) {
     auto search = m.find(str);
     return search != m.end();
 }
 
-bool inVector(const string& str, const vector<string>& m) {
+bool inVector(const string& str, const stringvec& m) {
     return (find(m.begin(), m.end(), str) != m.end());
 }
+
+string getResponse(string inputSentance) {
+    if (inMap(inputSentance, associationsDB)) {
+        stringvec l = (associationsDB.at(inputSentance));
+        return l[random_int(0, l.size() - 1)];
+    }
+    return "";
+}
+
+void read_directory(const string& name, stringvec& v, const string& searchFor = "")
+{
+    DIR* dirp = opendir(name.c_str());
+    struct dirent * dp;
+    while ((dp = readdir(dirp)) != NULL) {
+        string str = dp->d_name;
+        if (str.find(searchFor) != string::npos) {
+            v.push_back(str);
+        }
+    }
+    closedir(dirp);
+}
+
 
 void loadAxons() {
     cout << "Now loading Neurons..." << endl;
     // We are assuming we are in the root directory.
     ifstream f("./brain/asher.brain");
     if (f.good()) { // This means the file exists.
-        clock_t tStart = clock();
         int counter = 0;
-        vector<string> lines;
+        stringvec lines;
         string currentLine = "";
         while(getline(f, currentLine)) {
             // This is where we decide what to do with each line
@@ -54,7 +77,7 @@ void loadAxons() {
 
         for (auto item : lines) {
             // Working with each line to read the brain.
-            vector<string> line_part;
+            stringvec line_part;
             stringstream ss(item);
             string token;
             while(getline(ss, token, ':')) {
@@ -71,8 +94,8 @@ void loadAxons() {
                     (associationsDB.at(header)).push_back(response);
                 }
             } else {
-                vector<string> blank;
-                associationsDB.insert(pair<string, vector<string>>(header, blank));
+                stringvec blank;
+                associationsDB.insert(pair<string, stringvec>(header, blank));
                 // associationsDB[header] = vector<string>
                 (associationsDB.at(header)).push_back(response);
             }
@@ -83,10 +106,56 @@ void loadAxons() {
             counter++;
         }
         cout << "Finished with " << counter << " TOTAL ITERATIONS" << endl;
-        printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
-        associationsDB = map<string, vector<string>>();
-    } else { // We are going to have to manually load all the training files.
 
+    } else { // We are going to have to manually load all the training files.
+        //std::experimental::filesystem::recursive_directory_iterator
+        stringvec v;
+        read_directory("./training_data", v, ".yml");
+
+        int counter = 0;
+        for (string file : v) {
+
+            // Opening the file.
+            ifstream f("./training_data/" + file);
+            if (f.good()) { // This means the file successfully loaded
+                stringvec lines;
+                string currentLine = "";
+                while(getline(f, currentLine)) {
+                    lines.push_back(currentLine);
+                }
+
+                string lastHeader = "";
+
+                for (string item : lines) {
+                    if (item.find("- - ") != string::npos) {
+                        string holder = trim(item, "- - ", true);
+                        holder = trim(holder, "?", true);
+                        holder = trim(holder, ".", true);
+                        holder = trim(holder);
+
+                        lastHeader = holder;
+                    } else if (item.find("  - ") != string::npos) {
+                        string holder = trim(item, "  - ", true);
+                        holder = trim(holder);
+
+                        if (inMap(lastHeader, associationsDB)) {
+                            if (!inVector(lastHeader, associationsDB.at(lastHeader))) {
+                                (associationsDB.at(lastHeader)).push_back(holder);
+                            }
+                        } else {
+                            stringvec blank;
+                            associationsDB.insert(pair<string, stringvec>(lastHeader, blank));
+                            (associationsDB.at(lastHeader)).push_back(holder);
+                        }
+                    }
+                    if (counter % 10000 == 0) {
+                        cout << "Loaded " << counter << " neurons..." << endl;
+                    }
+                    counter++;
+                }
+            }
+        }
+        cout << "FINISHED WITH " << counter << " TOTAL NEURONS" << endl;
     }
 }
 
@@ -109,12 +178,12 @@ void start() {
 
 /**
  * What are these here for?
- * 
+ *
  * Well, to answer your question. It is alot faster for cpp to
  * handle all the brain work over javascript.
- * 
+ *
  * It also uses less memory.
- * 
+ *
  * So these are here to provide access to the brain that has been created on launch.
  **/
 
