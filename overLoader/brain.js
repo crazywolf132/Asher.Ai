4397bf7415e1d84a32470ef52eff1054177290a1
crazywolf132 <crazywolf132@gmail.com>
2019-03-21 11:52:11 +1100

    Removed lines from old system.

--- overLoader/brain.js
+++ overLoader/brain.js
@@ -1,22 +1,6 @@
-//import B from "../core/functions/latestBrain";
-// const B = require(`${process.cwd()}/core/functions/latestBrain`);
 import { workOut, startBrain } from "../build/Release/brain";
-//const newBrain = new B();
 
 export function core(payload, chat) {
-    /*let response = "";
-    response = newBrain.getResponse(
-        payload.found.includes("?") ?
-        payload.found.replace("?", "").toLowerCase() :
-        payload.found.toLowerCase());
-        
-    if (response == "-1") {
-        // This means we have an issue and need to work out what to do.
-        //response = thinking(chat, payload.found);
-        response = "Shiiit... idk how to respond to that. Sorry.";
-    }
-    chat.say(response);*/
-
     let response = "";
     response = workOut(payload.found.includes("?") ? payload.found.replace("?", "").toLowerCase() : payload.found.toLowerCase());
 
@@ -30,4 +14,3 @@ export function core(payload, chat) {
 export function preRun() {
     startBrain();
 }
-//🏢🏢✈💨 ☝🎅👍
